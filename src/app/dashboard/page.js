2ba16e45bef6b801f2e861578d74292a4e20eb24
MalicknND <ndiayemalick062@gmail.com>
2023-12-26 22:00:31 +0100

    swr

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import styles from "./page.module.scss";
+import useSWR from "swr";
 
 const Page = () => {
-  return <div>Dashboard</div>;
+  // const [data, setData] = useState([]);
+  // const [error, setError] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   async function getData() {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store",
+  //     });
+
+  //     if (!res.ok) {
+  //       setError(true);
+  //     }
+  //     const data = await res.json();
+  //     setData(data);
+  //     setIsLoading(false);
+  //   }
+  //   getData();
+  // }, []);
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+
+  console.log(data, error, isLoading);
+  return <div className={styles.container}>Dashboard</div>;
 };
 
 export default Page;
