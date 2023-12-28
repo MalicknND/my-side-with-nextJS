f865bfce7a13f665014d0bfd86aed593d216baad
Malick Siguy NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-12-28 14:12:28 +0000

    admin dashbord, add post

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,6 +4,7 @@ import styles from "./page.module.scss";
 import useSWR from "swr";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const Page = () => {
   // const [data, setData] = useState([]);
@@ -33,9 +34,11 @@ const Page = () => {
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading } = useSWR(
-    "https://jsonplaceholder.typicode.com/posts",
+    `/api/posts?username=${session?.data?.user.name}`,
     fetcher
   );
+  console.log(data);
+  //  `/api/posts?username=${data?.user.name}`
   if (session.status === "loading") {
     return <p>Loading...</p>;
   }
@@ -44,8 +47,66 @@ const Page = () => {
     router?.push("/dashboard/login");
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          desc,
+          img,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   if (session.status === "authenticated") {
-    return <div className={styles.container}>Dashboard</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {isLoading
+            ? "loading"
+            : data?.map((post) => (
+                <div className={styles.post} key={post._id}>
+                  <div className={styles.imgContainer}>
+                    <Image src={post.img} alt="" width={200} height={200} />
+                  </div>
+                  <h2 className={styles.postTitle}>{post.title}</h2>
+                  <span className={styles.delete}>X</span>
+                </div>
+              ))}
+        </div>
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add new post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input
+            type="text"
+            placeholder="Desccription"
+            className={styles.input}
+          />
+          <input type="text" placeholder="Image" className={styles.input} />
+          <textarea
+            placeholder="Content"
+            cols="30"
+            rows="10"
+            className={styles.textArea}
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
+      </div>
+    );
   }
 };
 
