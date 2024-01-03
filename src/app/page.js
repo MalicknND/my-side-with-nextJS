753cd9f2f6d4bb8ce08dec023cd1e534c2b4a03a
MalicknND <ndiayemalick062@gmail.com>
2024-01-04 00:18:46 +0100

    icons✅

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Hero from "/public/hero.png";
+import Hero from "/public/hero4.svg";
 import Button from "@/components/Button/Button";
 
 export default function Home() {
